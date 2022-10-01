9857e2a1d14711dc2e278c4cefeefc6873da8e73
AdrienWehrle <adrien.wehrle@hotmail.fr>
2022-10-01 09:36:21 +0200

    working on geomatch

--- src/CARRA_ERA5_match/match_CARRA_ERA5.py
+++ src/CARRA_ERA5_match/match_CARRA_ERA5.py
@@ -15,24 +15,6 @@ import matplotlib.pyplot as plt
 sys.path.append("/home/adrien/EO-IO/geomatcher")
 import geomatcher.geomatcher as gm
 
-# %% read ERA5 example
-
-grbs = pygrib.open(
-    "/home/adrien/EO-IO/CARRA_ERA5_events/data/ERA5/tcwv/202206_3hourly_tcwv.grib"
-)
-
-grbs.seek(0)
-for grb in grbs:
-    print(grb)
-
-selected_grb = grbs.select(name="Total column water vapour")[0]
-
-era5_ex, era5_lats, era5_lons = selected_grb.data()
-
-era5_grid = np.dstack([era5_ex, era5_lons, era5_lats])
-
-# %% read CARRA grid
-
 
 def lon360_to_lon180(lon360):
 
@@ -48,6 +30,26 @@ def lon360_to_lon180(lon360):
     return lon180
 
 
+# %% read ERA5 example
+
+grbs = pygrib.open(
+    "/home/adrien/EO-IO/CARRA_ERA5_events/data/ERA5/tcwv/202206_3hourly_tcwv.grib"
+)
+
+grbs.seek(0)
+for grb in grbs:
+    print(grb)
+
+selected_grb = grbs.select(name="Total column water vapour")[0]
+
+era5_ex, era5_lats, era5_lons_raw = selected_grb.data()
+era5_lons = lon360_to_lon180(era5_lons_raw)
+# era5_lons = era5_lons_raw
+
+era5_grid = np.dstack([era5_ex, era5_lons, era5_lats])
+
+# %% read CARRA grid
+
 fn = "/home/adrien/EO-IO/CARRA_rain/ancil/CARRA_W_elev_lat_lon.nc"
 
 carra_ds = xr.open_dataset(fn)
@@ -56,9 +58,26 @@ carra_elev = np.array(carra_ds["z"])[::-1]
 carra_lat = np.array(carra_ds["latitude"])[::-1]
 
 carra_lon_raw = np.array(carra_ds["longitude"])[::-1]
-carra_lon = lon360_to_lon180(carra_lon_raw)
+# carra_lon = lon360_to_lon180(carra_lon_raw)
+# carra_lon = carra_lon_raw
 
 carra_grid = np.dstack([carra_elev, carra_lon, carra_lat])
+
+# %%
+plt.figure()
+ax1 = plt.subplot(211)
+ax1.plot(era5_lons, era5_lats, alpha=0.1, color="gray")
+ax1.plot(carra_grid[:, :, 0], carra_grid[:, :, 1], alpha=0.1, color="red")
+ax2 = plt.subplot(212)
+ax2.imshow(era5_ex, aspect="auto")
+
+# %%
+
+plt.figure()
+ax1 = plt.subplot(211)
+ax1.imshow(era5_lons, aspect="auto")
+ax2 = plt.subplot(212)
+ax2.imshow(carra_lon, aspect="auto")
 
 # %% geomatch
 
@@ -67,4 +86,4 @@ indexes = gm.match_m2m(carra_grid, era5_grid, only_indexes=True)
 # %% check results
 
 plt.figure()
-plt.imshow(era5_grid.flatten()[indexes], vmin=0, vmax=20)
+plt.imshow(era5_ex.flatten()[indexes], vmin=0, vmax=5, aspect="auto")
